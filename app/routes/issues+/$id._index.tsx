5f888683020aa79dc0f2559e46856e7efb81e3d7
Jacob Paris <jacob@jacobparis.com>
2023-10-26 14:46:27 +0700

    feature: Add status, priority and save button

--- app/routes/issues+/$id._index.tsx
+++ app/routes/issues+/$id._index.tsx
@@ -3,7 +3,10 @@
 import { json, type LoaderFunctionArgs } from '@remix-run/node'
 import { Form, useLoaderData } from '@remix-run/react'
 
+import { SelectField } from '#app/components/forms.tsx'
+import { Button } from '#app/components/ui/button.tsx'
 import { Input } from '#app/components/ui/input.tsx'
+import { SelectGroup, SelectItem } from '#app/components/ui/select.tsx'
 import { Textarea } from '#app/components/ui/textarea.tsx'
 import { prisma } from '#app/utils/db.server.ts'
 
@@ -37,8 +40,46 @@ export default function Issue() {
 	const { issue } = useLoaderData<typeof loader>()
 
 	return (
-		<div className="mt-2 rounded-xl border px-2 py-2 shadow-sm">
-			<Form method="POST">
+		<Form method="POST">
+			<div className="mt-4 flex gap-x-2">
+				<SelectField
+					className="w-[200px]"
+					labelProps={{
+						children: 'Status',
+					}}
+					inputProps={{
+						defaultValue: issue.status ?? undefined,
+					}}
+				>
+					<SelectGroup>
+						{['todo', 'in-progress', 'done'].map(value => (
+							<SelectItem key={value} value={value}>
+								{value}
+							</SelectItem>
+						))}
+					</SelectGroup>
+				</SelectField>
+
+				<SelectField
+					className="w-[200px]"
+					labelProps={{
+						children: 'Priority',
+					}}
+					inputProps={{
+						defaultValue: issue.priority ?? undefined,
+					}}
+				>
+					<SelectGroup>
+						{['low', 'medium', 'high'].map(value => (
+							<SelectItem key={value} value={value}>
+								{value}
+							</SelectItem>
+						))}
+					</SelectGroup>
+				</SelectField>
+			</div>
+
+			<div className="mt-2 rounded-xl border px-2 py-2 shadow-sm">
 				<Input
 					aria-label="Title"
 					type="text"
@@ -58,7 +99,11 @@ export default function Issue() {
 					className="mt-2 border-none bg-transparent placeholder:text-gray-400"
 					defaultValue={issue.description ?? undefined}
 				/>
-			</Form>
-		</div>
+			</div>
+
+			<div className="mt-4 flex justify-end gap-x-2">
+				<Button type="submit">Save</Button>
+			</div>
+		</Form>
 	)
 }
