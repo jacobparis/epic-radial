8bf22cbf4a22dc79555306c17ed35fcd7b3f97a4
Jacob Paris <jacob@jacobparis.com>
2023-10-26 18:23:51 +0700

    feature: add conform to the create issue page

--- app/routes/index/route.tsx
+++ app/routes/index/route.tsx
@@ -1,14 +1,16 @@
 // http://localhost:3000/
 
-import { parse } from '@conform-to/zod'
+import { conform, useForm } from '@conform-to/react'
+import { getFieldsetConstraint, parse } from '@conform-to/zod'
 import {
 	json,
 	type ActionFunctionArgs,
 	type LoaderFunctionArgs,
 } from '@remix-run/node'
-import { Form, useLoaderData } from '@remix-run/react'
+import { Form, useActionData, useLoaderData } from '@remix-run/react'
 import { z } from 'zod'
 
+import { ErrorList } from '#app/components/forms.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import { Input } from '#app/components/ui/input.tsx'
 import { Textarea } from '#app/components/ui/textarea.tsx'
@@ -17,7 +19,7 @@ import { prisma } from '#app/utils/db.server.ts'
 import { IssuesTable } from './IssuesTable.tsx'
 
 const CreateIssueSchema = z.object({
-	title: z.string().nonempty(),
+	title: z.string({ required_error: 'Title is required' }).nonempty(),
 	description: z.string().optional(),
 })
 
@@ -46,7 +48,15 @@ export async function action({ request }: ActionFunctionArgs) {
 		},
 	})
 
-	return json({ success: true, submission })
+	return json({
+		success: true,
+		submission: {
+			...submission,
+			// When we send an empty payload, Conform resets the form to its default state
+			// Then we're ready to start on another issue
+			payload: null,
+		},
+	})
 }
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -66,6 +76,22 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export default function Dashboard() {
 	const { issues } = useLoaderData<typeof loader>()
+	const actionData = useActionData<typeof action>()
+
+	const [form, fields] = useForm({
+		id: 'create-issue-form',
+		// Adds required, min, etc props to the fields based on the schema
+		constraint: getFieldsetConstraint(CreateIssueSchema),
+		// Tells conform about any errors we've had
+		lastSubmission: actionData?.submission,
+		onValidate({ formData }) {
+			return parse(formData, { schema: CreateIssueSchema })
+		},
+		defaultValue: {
+			title: '',
+			description: '',
+		},
+	})
 
 	return (
 		<div className="mx-auto min-h-full max-w-4xl ">
@@ -75,22 +101,32 @@ export default function Dashboard() {
 
 			<div className="px-4 py-6">
 				<div className="rounded-xl border px-2 py-2 shadow-sm">
-					<Form method="POST">
+					<Form method="POST" {...form.props}>
 						<Input
 							aria-label="Title"
-							type="text"
-							name="title"
-							required
 							className="border-none bg-transparent text-lg font-medium placeholder:text-gray-400"
 							placeholder="Issue title"
+							{...conform.input(fields.title)}
 						/>
+						<div className="px-3">
+							<ErrorList
+								errors={fields.title.errors}
+								id={fields.title.errorId}
+							/>
+						</div>
 
 						<Textarea
 							aria-label="Description"
-							name="description"
 							placeholder="Add a description…"
 							className="mt-2 border-none bg-transparent placeholder:text-gray-400"
+							{...conform.input(fields.description)}
 						/>
+						<div className="px-3">
+							<ErrorList
+								errors={fields.description.errors}
+								id={fields.description.errorId}
+							/>
+						</div>
 
 						<div className="mt-4 flex justify-end">
 							<Button type="submit">Save</Button>
